5321aff9b3e5d10036a4098cf7484582f820db58
Azodox_ <likemoi99@gmail.com>
2022-04-02 15:53:42 +0200

    Add reloadPermissions and set methods.

--- src/main/java/net/valneas/account/permission/PermissionDispatcher.java
+++ src/main/java/net/valneas/account/permission/PermissionDispatcher.java
@@ -4,6 +4,8 @@ import net.valneas.account.AccountSystem;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.Set;
+
 public class PermissionDispatcher {
 
     private final AccountSystem accountSystem;
@@ -14,6 +16,23 @@ public class PermissionDispatcher {
 
     public void onEnable(){
         Bukkit.getOnlinePlayers().forEach(this.accountSystem.getPermissionDatabase()::setPlayerPermission);
+    }
+
+    public void reloadPermissions() {
+        this.onEnable();
+    }
+
+    public void reloadPermissions(Player player){
+        this.accountSystem.getPermissionDatabase().setPlayerPermission(player);
+    }
+
+    public void set(Player player, String permission, boolean state){
+        this.accountSystem.getPermissionDatabase().update(new PermissionDatabase.Permission(
+                state ? permission : '-' + permission,
+                Set.of(player.getUniqueId()),
+                null
+        ));
+        reloadPermissions(player);
     }
 
     public void set(Player player){
