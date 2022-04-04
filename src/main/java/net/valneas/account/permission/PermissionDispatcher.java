b76ce40c9ee31648c36fa8f1e5f604ce4b6f1a60
Azodox_ <likemoi99@gmail.com>
2022-04-04 19:48:40 +0200

    Replaced Player object in parameters by Object in reloadPermissions and set methods. Added remove method (not finished yet)

--- src/main/java/net/valneas/account/permission/PermissionDispatcher.java
+++ src/main/java/net/valneas/account/permission/PermissionDispatcher.java
@@ -1,10 +1,13 @@
 package net.valneas.account.permission;
 
+import net.valneas.account.AccountManager;
 import net.valneas.account.AccountSystem;
+import net.valneas.account.rank.RankUnit;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.Set;
+import java.util.UUID;
 
 public class PermissionDispatcher {
 
@@ -15,36 +18,61 @@ public class PermissionDispatcher {
     }
 
     public void onEnable(){
-        Bukkit.getOnlinePlayers().forEach(this.accountSystem.getPermissionDatabase()::setPlayerPermission);
+        Bukkit.getOnlinePlayers().forEach(this::reloadPermissions);
     }
 
     public void reloadPermissions() {
         this.onEnable();
     }
 
-    public void reloadPermissions(Player player){
-        this.accountSystem.getPermissionDatabase().setPlayerPermission(player);
+    public void reloadPermissions(Object target){
+        if(target instanceof Player player) {
+            this.accountSystem.getPermissionDatabase().setPlayerPermission(player);
+        }else if(target instanceof UUID uuid){
+            this.accountSystem.getPermissionDatabase().setPlayerPermission(Bukkit.getPlayer(uuid));
+        }else if(target instanceof RankUnit rankUnit){
+            Bukkit.getOnlinePlayers().forEach(player -> {
+                var account = new AccountManager(this.accountSystem, player);
+                var rank = account.newRankManager();
+
+                if(rank.hasRank(rankUnit)){
+                    this.accountSystem.getPermissionDatabase().setPlayerPermission(player);
+                }
+            });
+        }
     }
 
-    public void set(Player player, String permission, boolean state){
-        if(!state){
-            permission = '-' + permission;
-        }
-
+    public void set(Object target, String permission){
         var dbPermission = this.accountSystem.getPermissionDatabase().get(permission);
 
         if(dbPermission == null){
             this.accountSystem.getPermissionDatabase().update(new PermissionDatabase.Permission(
                     permission,
-                    Set.of(player.getUniqueId()),
-                    null,
+                    target instanceof Player player ? Set.of(player.getUniqueId()) : target instanceof UUID uuid ? Set.of(uuid) : null,
+                    target instanceof RankUnit rankUnit ? Set.of(rankUnit) : null,
                     null
             ));
         }else{
-            dbPermission.players().add(player.getUniqueId());
+            if(target instanceof Player player) {
+                dbPermission.players().add(player.getUniqueId());
+            }else if(target instanceof UUID uuid){
+                dbPermission.players().add(uuid);
+            }else if(target instanceof RankUnit rankUnit){
+                dbPermission.ranks().add(rankUnit);
+            }
             this.accountSystem.getPermissionDatabase().update(dbPermission);
         }
-        reloadPermissions(player);
+        reloadPermissions(target);
+    }
+
+    public void remove(Object target, String permission){
+        var dbPermission = this.accountSystem.getPermissionDatabase().get(permission);
+
+        if(dbPermission == null){
+            return;
+        }
+
+
     }
 
     public void set(Player player){
