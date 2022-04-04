44bb78ae5434d68f912f66d8f5cbc3ce17a7f827
Azodox_ <likemoi99@gmail.com>
2022-04-04 19:49:20 +0200

    Using command-builder instead of vanilla way of creating command.

--- src/main/java/net/valneas/account/api/commands/PermissionCommand.java
+++ src/main/java/net/valneas/account/api/commands/PermissionCommand.java
@@ -1,13 +1,17 @@
 package net.valneas.account.api.commands;
 
+import io.github.llewvallis.commandbuilder.CommandContext;
+import io.github.llewvallis.commandbuilder.ExecuteCommand;
+import io.github.llewvallis.commandbuilder.OptionalArg;
+import io.github.llewvallis.commandbuilder.arguments.StringSetArgument;
 import net.valneas.account.AccountSystem;
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import net.valneas.account.permission.PermissionDatabase;
+import net.valneas.account.rank.RankUnit;
 import org.bukkit.entity.Player;
 
-public class PermissionCommand implements CommandExecutor {
+import java.util.UUID;
+
+public class PermissionCommand {
 
     private final AccountSystem accountSystem;
 
@@ -15,41 +19,37 @@ public class PermissionCommand implements CommandExecutor {
         this.accountSystem = accountSystem;
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
-
-        if (args.length == 0) {
+    @ExecuteCommand
+    public boolean permission(CommandContext ctx, @StringSetArgument.Arg({"add", "remove", "show", "reload"}) String operation, String targetInput, @OptionalArg PermissionDatabase.Permission permission) {
+        var target = PermissionDatabase.DatabaseParser.parse(targetInput);
+        if(target == null){
             return true;
         }
 
-        if(args[0].equalsIgnoreCase("add")){
-            if(args.length > 3){
-                return true;
+        if(operation.equalsIgnoreCase("add")){
+            this.accountSystem.getPermissionDispatcher().set(target, permission.permission());
+            ctx.getSender().sendMessage("Good");
+            return true;
+        }
+
+        if(operation.equalsIgnoreCase("remove")){
+
+        }
+
+        if(operation.equalsIgnoreCase("reload")){
+            this.accountSystem.getPermissionDispatcher().reloadPermissions(target);
+            ctx.getSender().sendMessage("Permissions de '" + target + "' rechargées ✔");
+            return true;
+        }
+
+        if(operation.equalsIgnoreCase("show")){
+            if(target instanceof Player player) {
+                this.accountSystem.getPermissionDatabase().getUUIDPermissions(player.getUniqueId()).forEach(perm -> ctx.getSender().sendMessage(perm.permission()));
+            }else if(target instanceof UUID uuid){
+                this.accountSystem.getPermissionDatabase().getUUIDPermissions(uuid).forEach(perm -> ctx.getSender().sendMessage(perm.permission()));
+            }else if(target instanceof RankUnit rankUnit){
+                this.accountSystem.getPermissionDatabase().getRankPermissions(rankUnit).forEach(perm -> ctx.getSender().sendMessage(perm.permission()));
             }
-
-            Player target = Bukkit.getPlayer(args[1]);
-            if(target == null){
-                return true;
-            }
-
-            this.accountSystem.getPermissionDispatcher().set(target, args[2], true);
-            sender.sendMessage("Good");
-            return true;
-        }
-
-        if(args.length >= 2){
-            return true;
-        }
-
-        if(args[0].equalsIgnoreCase("reload")){
-            this.accountSystem.getPermissionDispatcher().reloadPermissions();
-            sender.sendMessage("Permissions rechargées ✔");
-            return true;
-        }
-
-        Player target = Bukkit.getPlayer(args[0]);
-        if(target != null){
-            target.getEffectivePermissions().forEach(permission -> sender.sendMessage(permission.getPermission() + ", " + permission.getValue()));
         }
 
         return false;
