909ed3b18cda9598af449f63f083799f991bdbf0
Azodox_ <likemoi99@gmail.com>
2022-04-20 18:10:28 +0200

    Change compatibility to paper only.

--- src/main/java/net/valneas/account/rank/RankUnit.java
+++ src/main/java/net/valneas/account/rank/RankUnit.java
@@ -1,6 +1,7 @@
 package net.valneas.account.rank;
 
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,27 +9,27 @@ import java.util.List;
 
 public enum RankUnit {
 
-    ADMIN(1, 1, "Admin", net.md_5.bungee.api.ChatColor.of("#FDF617") + "\u2730 " + ChatColor.DARK_RED + "Admin ", "", ChatColor.DARK_RED),
-    HEAD(2, 2, "Head", "Head-", "", ChatColor.BOLD),
-    DEVELOPPEUR(3, 3, "Développeur", "§c§oDéveloppeur ", "", ChatColor.RED),
-    DEVELOPPEUSE(4, 4, "Développeuse", "§c§oDéveloppeuse ", "", ChatColor.RED),
-    BUILDER(5, 5, "Builder", "§bBuilder ", "", ChatColor.AQUA),
-    BUILDEUSE(6, 6, "Buildeuse", "§bBuildeuse ", "", ChatColor.AQUA),
-    MODERATEUR(7, 7, "Modérateur", "§2Modérateur ", "", ChatColor.DARK_GREEN),
-    MODERATRICE(8, 8, "Modératrice", "§2Modératrice ", "", ChatColor.DARK_GREEN),
-    ASSISTANT(9, 9, "Assistant", "§aAssistant ", "", ChatColor.GREEN),
-    ASSISTANTE(10, 10, "Assistante", "§aAssistante ", "", ChatColor.GREEN),
-    STAFF(11, 15, "Staff", "§d\u2726§o Staff ", "", ChatColor.LIGHT_PURPLE),
-    PARTENAIRE(12, 11, "Partenaire", "§6Partenaire ", "", ChatColor.GOLD),
-    YOUTUBE(13, 12, "YouTube", "§c§nYouTube ", "", ChatColor.RED),
-    TWITCH(14, 13, "Twitch", "§5§nTwitch ", "", ChatColor.DARK_PURPLE),
-    JOUEUR(15, 14, "Joueur", "§7", "", ChatColor.GRAY);
+    ADMIN(1, 1, "Admin", TextColor.fromHexString("#FDF617") + "\u2730 " + NamedTextColor.DARK_RED + "Admin ", "", NamedTextColor.DARK_RED),
+    HEAD(2, 2, "Head", "Head-", "", NamedTextColor.WHITE),
+    DEVELOPPEUR(3, 3, "Développeur", "§c§oDéveloppeur ", "", NamedTextColor.RED),
+    DEVELOPPEUSE(4, 4, "Développeuse", "§c§oDéveloppeuse ", "", NamedTextColor.RED),
+    BUILDER(5, 5, "Builder", "§bBuilder ", "", NamedTextColor.AQUA),
+    BUILDEUSE(6, 6, "Buildeuse", "§bBuildeuse ", "", NamedTextColor.AQUA),
+    MODERATEUR(7, 7, "Modérateur", "§2Modérateur ", "", NamedTextColor.DARK_GREEN),
+    MODERATRICE(8, 8, "Modératrice", "§2Modératrice ", "", NamedTextColor.DARK_GREEN),
+    ASSISTANT(9, 9, "Assistant", "§aAssistant ", "", NamedTextColor.GREEN),
+    ASSISTANTE(10, 10, "Assistante", "§aAssistante ", "", NamedTextColor.GREEN),
+    STAFF(11, 15, "Staff", "§d\u2726§o Staff ", "", NamedTextColor.LIGHT_PURPLE),
+    PARTENAIRE(12, 11, "Partenaire", "§6Partenaire ", "", NamedTextColor.GOLD),
+    YOUTUBE(13, 12, "YouTube", "§c§nYouTube ", "", NamedTextColor.RED),
+    TWITCH(14, 13, "Twitch", "§5§nTwitch ", "", NamedTextColor.DARK_PURPLE),
+    JOUEUR(15, 14, "Joueur", "§7", "", NamedTextColor.GRAY);
 
     private final String name, prefix, suffix;
     private final int power, id;
-    private final ChatColor color;
+    private final NamedTextColor color;
 
-    RankUnit(int power, int id, String name, String prefix, String suffix, ChatColor color){
+    RankUnit(int power, int id, String name, String prefix, String suffix, NamedTextColor color){
         this.power = power;
         this.id = id;
         this.name = name;
@@ -57,7 +58,7 @@ public enum RankUnit {
         return id;
     }
 
-    public ChatColor getColor() {
+    public NamedTextColor getColor() {
         return color;
     }
 
