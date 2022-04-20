2e1022ccbcc86a6363e44f58a41f1c90e5c4942e
Azodox_ <likemoi99@gmail.com>
2022-04-20 18:40:58 +0200

    Fixed ranks prefixes and suffixes compatibility with Paper.

--- src/main/java/net/valneas/account/rank/RankUnit.java
+++ src/main/java/net/valneas/account/rank/RankUnit.java
@@ -1,7 +1,9 @@
 package net.valneas.account.rank;
 
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,27 +11,28 @@ import java.util.List;
 
 public enum RankUnit {
 
-    ADMIN(1, 1, "Admin", TextColor.fromHexString("#FDF617") + "\u2730 " + NamedTextColor.DARK_RED + "Admin ", "", NamedTextColor.DARK_RED),
-    HEAD(2, 2, "Head", "Head-", "", NamedTextColor.WHITE),
-    DEVELOPPEUR(3, 3, "Développeur", "§c§oDéveloppeur ", "", NamedTextColor.RED),
-    DEVELOPPEUSE(4, 4, "Développeuse", "§c§oDéveloppeuse ", "", NamedTextColor.RED),
-    BUILDER(5, 5, "Builder", "§bBuilder ", "", NamedTextColor.AQUA),
-    BUILDEUSE(6, 6, "Buildeuse", "§bBuildeuse ", "", NamedTextColor.AQUA),
-    MODERATEUR(7, 7, "Modérateur", "§2Modérateur ", "", NamedTextColor.DARK_GREEN),
-    MODERATRICE(8, 8, "Modératrice", "§2Modératrice ", "", NamedTextColor.DARK_GREEN),
-    ASSISTANT(9, 9, "Assistant", "§aAssistant ", "", NamedTextColor.GREEN),
-    ASSISTANTE(10, 10, "Assistante", "§aAssistante ", "", NamedTextColor.GREEN),
-    STAFF(11, 15, "Staff", "§d\u2726§o Staff ", "", NamedTextColor.LIGHT_PURPLE),
-    PARTENAIRE(12, 11, "Partenaire", "§6Partenaire ", "", NamedTextColor.GOLD),
-    YOUTUBE(13, 12, "YouTube", "§c§nYouTube ", "", NamedTextColor.RED),
-    TWITCH(14, 13, "Twitch", "§5§nTwitch ", "", NamedTextColor.DARK_PURPLE),
-    JOUEUR(15, 14, "Joueur", "§7", "", NamedTextColor.GRAY);
+    ADMIN(1, 1, "Admin", Component.text("\u2730 ").color(TextColor.fromHexString("#FDF617")).append(Component.text("Admin ").color(NamedTextColor.DARK_RED)), Component.empty(), NamedTextColor.DARK_RED),
+    HEAD(2, 2, "Head", Component.text("Head-"), Component.empty(), NamedTextColor.WHITE),
+    DEVELOPPEUR(3, 3, "Développeur", Component.text("Développeur ").color(NamedTextColor.RED).decorate(TextDecoration.ITALIC).append(Component.space()), Component.empty(), NamedTextColor.RED),
+    DEVELOPPEUSE(4, 4, "Développeuse", Component.text("Développeuse").color(NamedTextColor.RED).decorate(TextDecoration.ITALIC).append(Component.space()), Component.empty(), NamedTextColor.RED),
+    BUILDER(5, 5, "Builder", Component.text("Builder").color(NamedTextColor.AQUA).append(Component.space()), Component.empty(), NamedTextColor.AQUA),
+    BUILDEUSE(6, 6, "Buildeuse", Component.text("Buildeuse").color(NamedTextColor.AQUA).append(Component.space()), Component.empty(), NamedTextColor.AQUA),
+    MODERATEUR(7, 7, "Modérateur", Component.text("Modérateur").color(NamedTextColor.DARK_GREEN).append(Component.space()), Component.empty(), NamedTextColor.DARK_GREEN),
+    MODERATRICE(8, 8, "Modératrice", Component.text("Modératrice").color(NamedTextColor.DARK_GREEN).append(Component.space()), Component.empty(), NamedTextColor.DARK_GREEN),
+    ASSISTANT(9, 9, "Assistant", Component.text("Assistant").color(NamedTextColor.GREEN).append(Component.space()), Component.empty(), NamedTextColor.GREEN),
+    ASSISTANTE(10, 10, "Assistante", Component.text("Assistante").color(NamedTextColor.GREEN).append(Component.space()), Component.empty(), NamedTextColor.GREEN),
+    STAFF(11, 15, "Staff", Component.text("\u2726 Staff").color(NamedTextColor.LIGHT_PURPLE).decorate(TextDecoration.ITALIC).append(Component.space()), Component.empty(), NamedTextColor.LIGHT_PURPLE),
+    PARTENAIRE(12, 11, "Partenaire", Component.text("Partenaire").color(NamedTextColor.GOLD).append(Component.space()), Component.empty(), NamedTextColor.GOLD),
+    YOUTUBE(13, 12, "YouTube", Component.text("YouTube").color(NamedTextColor.RED).decorate(TextDecoration.UNDERLINED).append(Component.space()), Component.empty(), NamedTextColor.RED),
+    TWITCH(14, 13, "Twitch", Component.text("Twitch").color(NamedTextColor.DARK_PURPLE).decorate(TextDecoration.UNDERLINED).append(Component.space()), Component.empty(), NamedTextColor.DARK_PURPLE),
+    JOUEUR(15, 14, "Joueur", Component.text("").color(NamedTextColor.GRAY), Component.empty(), NamedTextColor.GRAY);
 
-    private final String name, prefix, suffix;
+    private final String name;
+    private final Component prefix, suffix;
     private final int power, id;
     private final NamedTextColor color;
 
-    RankUnit(int power, int id, String name, String prefix, String suffix, NamedTextColor color){
+    RankUnit(int power, int id, String name, Component prefix, Component suffix, NamedTextColor color){
         this.power = power;
         this.id = id;
         this.name = name;
@@ -42,11 +45,11 @@ public enum RankUnit {
         return name;
     }
 
-    public String getPrefix() {
+    public Component getPrefix() {
         return prefix;
     }
 
-    public String getSuffix() {
+    public Component getSuffix() {
         return suffix;
     }
 
