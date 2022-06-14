9eed414df3c5c0a03fd5eb6d3a851f66286739d2
Azodox_ <likemoi99@gmail.com>
2022-06-14 02:04:19 +0200

    Replaced PermissionDatabase.Permission with Permission.java

--- paper/src/main/java/net/valneas/account/commands/SetDefaultCommand.java
+++ paper/src/main/java/net/valneas/account/commands/SetDefaultCommand.java
@@ -3,24 +3,24 @@ package net.valneas.account.commands;
 import io.github.llewvallis.commandbuilder.CommandContext;
 import io.github.llewvallis.commandbuilder.ExecuteCommand;
 import net.valneas.account.AccountSystem;
-import net.valneas.account.permission.PermissionDatabase;
+import net.valneas.account.permission.Permission;
 
 public class SetDefaultCommand {
 
-    private AccountSystem accountSystem;
+    private final AccountSystem accountSystem;
 
     public SetDefaultCommand(AccountSystem accountSystem) {
         this.accountSystem = accountSystem;
     }
 
     @ExecuteCommand
-    public void setDefault(CommandContext ctx, PermissionDatabase.Permission permission, boolean value) {
+    public void setDefault(CommandContext ctx, Permission permission, boolean value) {
         if(value){
-            this.accountSystem.getPermissionDispatcher().setDefault(permission.permission());
-            ctx.getSender().sendMessage("'" + permission.permission() + "' définie comme par défaut ✔");
+            this.accountSystem.getPermissionDispatcher().setDefault(permission.getPermission());
+            ctx.getSender().sendMessage("'" + permission.getPermission() + "' définie comme par défaut ✔");
         }else{
-            this.accountSystem.getPermissionDispatcher().unsetDefault(permission.permission());
-            ctx.getSender().sendMessage("'" + permission.permission() + "' définie comme non par défaut ✔");
+            this.accountSystem.getPermissionDispatcher().unsetDefault(permission.getPermission());
+            ctx.getSender().sendMessage("'" + permission.getPermission() + "' définie comme non par défaut ✔");
         }
     }
 }
