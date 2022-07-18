683b612b174cb94c1920a3d5b3bd342b16dae856
Azodox_ <likemoi99@gmail.com>
2022-07-18 23:10:32 +0200

    Renamed RankHandler.java

--- paper/src/main/java/net/valneas/account/rank/PaperRankHandler.java
+++ paper/src/main/java/net/valneas/account/rank/PaperRankHandler.java
@@ -7,19 +7,17 @@ import net.kyori.adventure.text.JoinConfiguration;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RankHandler extends AbstractRankHandler<RankUnit> {
+public class PaperRankHandler extends AbstractRankHandler<PaperRankUnit> {
 
-    public RankHandler(Datastore datastore) {
-        super(datastore, RankUnit.class);
+    public PaperRankHandler(Datastore datastore) {
+        super(datastore, PaperRankUnit.class);
     }
 
     public Component getRankList(){
         List<Component> ranks = new ArrayList<>();
 
-        for (AbstractRankUnit next : getAllRanksQuery()) {
-            if (next instanceof RankUnit rank) {
-                ranks.add(rank.name().color(rank.getColor()));
-            }
+        for (PaperRankUnit rank : getAllRanksQuery()) {
+            ranks.add(rank.name().color(rank.color()));
         }
 
         return Component.join(JoinConfiguration.commas(true), ranks);
